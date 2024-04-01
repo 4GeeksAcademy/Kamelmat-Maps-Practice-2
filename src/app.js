fc765bfd47a0beed5b3a32071f3f578f0c30194b
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2024-04-01 10:28:29 +0000

    Almost done 3rd variable

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 import spades from "./assets/img/01_of_spades_A.svg.png";
 import diamonds from "./assets/img/200px-Playing_card_diamond_A.svg.png";
 import clubs from "./assets/img/640px-Ace_of_clubs.svg.png";
@@ -12,62 +10,31 @@ import hearts from "./assets/img/Ace_of_hearts.svg.png";
 window.onload = function () {
   //write your code here
 
-  // PRIMERA OPCIÓN FUNCIONANDO:
-
-  /*   let palos = ['♦', '♥', '♠', '♣'];
-    let cartas = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-  
-    let textMap = '<ul class="list-group">'
-    textMap += '<li class="list-group-item bg-success text-light">Using .map()</li>'
-  
-    palos.map((iterator) => {
-      cartas.map((item) => {
-        textMap += `
-           <li class="list-group-item">
-             ${iterator}${item}
-           </li>
-         `
-      })
-    })
-  
-  
-    textMap += '</ul>';
-    document.querySelector('#poker').innerHTML = textMap;
-  };
-   */
-
-
-  // SEGUNDA OPCIÓN CON IMAGENES:
 
   let palos = ['♦', '♥', '♠', '♣'];
+  let pics = [diamonds, hearts, spades, clubs];
   let cartas = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-  let pics = { diamonds, hearts, spades, clubs };
 
-  
-
-/*   palos.map((iterator) => {
-    cartas.map((item) => {
-      Object.keys(pics).map((key) => {
-        let element = pics[key];
+  let textMap = '<ul class="list-group">'
+  textMap += '<li class="list-group-it∫em bg-success text-light">Poker Cards</li>'
+  palos.map((iterator) => {
+    pics.map((item) => {
+      cartas.map((element) => {
         textMap += `
-        <li class="list-group-item">
-        ${iterator}${item}<img src="${element}" alt="${key}">
-        </li>
-      `
+        <div class="container d-flex justify-content-center">
+           <div class="card" style="width: 18rem;">
+            <img src="${item}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${iterator}</h5>
+              <p class="card-text">${element}</p>
+            </div>
+          </div>
+        </div>
+        `
+
       })
     })
-  }) */
-  let textMap = '<ul class="list-group">'
-  textMap += '<li class="list-group-it∫em bg-success text-light">Using .map()</li>'
-
-  let resultado = cartas.map((elemento2, indice) => {
-    let elemento1 = palos[indice]; // Obtener el elemento correspondiente de let1
-    let elemento3 = pics[elemento1]; // Obtener el elemento correspondiente de pics
-    return elemento1 + elemento2 + `<img src="${elemento3}" alt="${elemento1}${elemento2}">`; // Combinar elementos de let1, let2 y pics y devolverlos
-  });
-
-  textMap += resultado.join(""); // Unir todos los elementos del arreglo resultado y agregarlos a textMap
-
+  })
   textMap += '</ul>';
   document.querySelector('#poker').innerHTML = textMap;
 }
